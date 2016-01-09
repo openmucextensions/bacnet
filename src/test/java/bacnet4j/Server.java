3b02f9a4462fe735c04ae94a1d8eba5a9f39c9d1
Mike <Pichler@MacBook.local>
2016-01-09 15:15:03 +0100

    Updated server test

--- src/test/java/bacnet4j/Server.java
+++ src/test/java/bacnet4j/Server.java
@@ -2,11 +2,13 @@ package bacnet4j;
 
 import com.serotonin.bacnet4j.LocalDevice;
 import com.serotonin.bacnet4j.npdu.ip.IpNetwork;
+import com.serotonin.bacnet4j.npdu.ip.IpNetworkBuilder;
 import com.serotonin.bacnet4j.obj.BACnetObject;
 import com.serotonin.bacnet4j.transport.DefaultTransport;
 import com.serotonin.bacnet4j.transport.Transport;
-import com.serotonin.bacnet4j.type.constructed.PropertyValue;
+import com.serotonin.bacnet4j.type.constructed.StatusFlags;
 import com.serotonin.bacnet4j.type.enumerated.EngineeringUnits;
+import com.serotonin.bacnet4j.type.enumerated.EventState;
 import com.serotonin.bacnet4j.type.enumerated.ObjectType;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
 import com.serotonin.bacnet4j.type.primitive.CharacterString;
@@ -15,10 +17,13 @@ import com.serotonin.bacnet4j.type.primitive.Real;
 
 public class Server {
 	
+	public static final int port = 0xBAC5;
+	
 	public static void main(String[] args) throws Throwable {
 		
-		IpNetwork network = new IpNetwork("10.78.20.255", 0xBAC5);
-        Transport transport = new DefaultTransport(network);
+		// IpNetwork network = new IpNetwork("10.78.20.255", 0xBAC5);
+		IpNetwork network = new IpNetworkBuilder().port(port).build();
+		Transport transport = new DefaultTransport(network);
         
         int localDeviceID = 10000 + (int) ( Math.random() * 10000);
         LocalDevice localDevice = new LocalDevice(localDeviceID, transport);
@@ -27,16 +32,18 @@ public class Server {
         System.out.println("Local device is running with device id " + localDeviceID);
         
         ObjectIdentifier objectId = new ObjectIdentifier(ObjectType.analogValue, 1);
-// TODO
-//        BACnetObject object = new BACnetObject(localDevice, objectId);
-//        
-//        object.setProperty(new PropertyValue(PropertyIdentifier.presentValue, new Real(12.3f)));
-//        object.setProperty(new PropertyValue(PropertyIdentifier.description, new CharacterString("Temperaturwert")));
-//        object.setProperty(new PropertyValue(PropertyIdentifier.units, EngineeringUnits.degreesCelsius));
-//        object.setProperty(new PropertyValue(PropertyIdentifier.objectName, new CharacterString("B'U'TOa")));
+
+        // BACnetObject object = new BACnetObject(localDevice, objectId);
+        BACnetObject object = new BACnetObject(objectId, "B'U'TOa");
         
-        
-//        localDevice.addObject(object);
+        object.writeProperty(PropertyIdentifier.presentValue, new Real(12.3f));
+        object.writeProperty(PropertyIdentifier.description, new CharacterString("Temperaturwert"));
+        object.writeProperty(PropertyIdentifier.units, EngineeringUnits.degreesCelsius);
+        object.writeProperty(PropertyIdentifier.statusFlags, new StatusFlags(false, false, false, false));
+        object.writeProperty(PropertyIdentifier.eventState, EventState.normal);
+        object.writeProperty(PropertyIdentifier.outOfService, new com.serotonin.bacnet4j.type.primitive.Boolean(false));
+
+        localDevice.addObject(object);
  
 		
 	}
